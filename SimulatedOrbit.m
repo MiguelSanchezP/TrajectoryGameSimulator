6fcc0a3238e94b352c8f5aee3248cf0ed39a1d04
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2019-01-31 16:31:59 +0100

    Representation of the center and initial vector

--- SimulatedOrbit.m
+++ SimulatedOrbit.m
@@ -17,4 +17,10 @@ printf("Initial Vector's Final Y: ");
 variableYtemp=input(' ');
 points=[points;variableXtemp variableYtemp];
 %all data necessary has been introduced to the system
-plot([points(1,1) points(2,1)],[points(1,2) points(2,2)], 'r*')
+axis on;
+plot([points(1,1) points(2,1)],[points(1,2) points(2,2)], 'r')
+hold on;
+plot([centre(1,1)],[centre(1,2)], 'ro')
+axis equal;
+axis([-10 10 -10 10]);
+%Representation of the initial vector and the center point
