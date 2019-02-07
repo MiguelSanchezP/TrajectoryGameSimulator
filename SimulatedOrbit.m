6b709aad81a8f0d0366f145be01ee4b7c3339be5
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2019-02-07 01:01:59 +0100

    Add files via upload
    done the mathematical approach of the points with accelerations

--- SimulatedOrbit.m
+++ SimulatedOrbit.m
@@ -25,19 +25,34 @@ plot([centre(1,1)],[centre(1,2)], 'ro')
 axis equal;
 axis([-10 10 -10 10]);
 %Representation of the initial vector and the center point
-finalVec=[points(2,1) points(2,2)];
-theta1=atan(finalVec(1,1)/finalVec(1,2));
-a1=[0 0];
-if (theta1<=1.178097 && theta1>=0.3926991)
-  a1=[1 1];
-elseif (theta1>=0 && theta1<0.3926991)
-  a1=[1 0];
-elseif (theta1<=1.570796 && theta1>1.178097)
-  a1=[0 1];
-endif
-if (finalVec(1,2)>0)
-  a1(1,2)=a1(1,2)*-1;
-endif
-if (finalVec(1,1)>0)
-  a1(1,1)=a1(1,1)*-1;
-endif
+  InitialPoint=[points(1,1) points(1,2)];
+  FinalPoint=[points(2,1) points(2,2)];
+  accelerations=[];
+for i=1:3
+  theta1=atan(FinalPoint(1,1)/FinalPoint(1,2));
+  a1=[0 0];
+  if (theta1<0)
+    theta1=theta1*-1;
+  endif
+  if (theta1<=1.178097 && theta1>=0.3926991)
+    a1=[1 1];
+  elseif (theta1>=0 && theta1<0.3926991)
+    a1=[1 0];
+  elseif (theta1<=1.570796 && theta1>1.178097)
+    a1=[0 1];
+  endif
+  if (FinalPoint(1,2)>0)
+    a1(1,2)=a1(1,2)*-1;
+  endif
+  if (FinalPoint(1,1)>0)
+    a1(1,1)=a1(1,1)*-1;
+  endif
+%  InitialPoint=FinalPoint;
+  TempPoint=FinalPoint;
+  FinalPoint=[(FinalPoint(1,1)+(FinalPoint(1,1)-InitialPoint(1,1))+a1(1,1)) (FinalPoint(1,2)+(FinalPoint(1,2)-InitialPoint(1,2))+a1(1,2))];
+  InitialPoint=TempPoint;
+  %  FinalPoint=[(InitialPoint(1,1)+a1(1,1)) (InitialPoint(1,2)+a1(1,2))];
+  
+  points=[points;FinalPoint];
+  accelerations=[accelerations;a1];
+endfor
