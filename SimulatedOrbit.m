7c13141c83d94645743670f11f67c053830f2d16
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2019-02-09 14:24:42 +0100

    Finished the algorithm, code is working now :)

--- SimulatedOrbit.m
+++ SimulatedOrbit.m
@@ -28,31 +28,82 @@ axis([-10 10 -10 10]);
   InitialPoint=[points(1,1) points(1,2)];
   FinalPoint=[points(2,1) points(2,2)];
   accelerations=[];
-for i=1:3
-  theta1=atan(FinalPoint(1,1)/FinalPoint(1,2));
-  a1=[0 0];
-  if (theta1<0)
-    theta1=theta1*-1;
+##for i=1:10
+##  %make it with the sides longitudes, not with the point position, create two additional variables storing the length
+##  %theta1=atan(FinalPoint(1,1)/FinalPoint(1,2));
+##  %tempYVal=FinalPoint(1,2);
+##  distanceX=FinalPoint(1,1)-InitialPoint(1,1);
+##  distanceY=FinalPoint(1,2)-InitialPoint(1,2);
+##  %if tempYVal<0
+##  %  tempYVal=tempYVal*-1;
+##  %endif
+##  %theta1=asin(tempYVal/(sqrt(power(FinalPoint(1,1),2)+power(FinalPoint(1,2),2)))); % make with tangent (use length)wolf
+##  theta1=atan(distanceY/distanceX);
+##  a1=[0 0];
+##  if (theta1<0)
+##    theta1=theta1*-1;
+##  endif
+##  if (theta1<=1.178097 && theta1>=0.3926991)
+##    a1=[1 1];
+##  elseif (theta1>=0 && theta1<0.3926991)
+##    a1=[1 0];
+##  elseif (theta1<=1.570796 && theta1>1.178097)
+##    a1=[0 1];
+##  endif
+##  if (FinalPoint(1,2)>0)
+##    a1(1,2)=a1(1,2)*-1;
+##  endif
+##  if (FinalPoint(1,1)>0)
+##    a1(1,1)=a1(1,1)*-1;
+##  endif
+##%  InitialPoint=FinalPoint;
+##  TempPoint=FinalPoint;
+##  FinalPoint=[(TempPoint(1,1)+distanceX+a1(1,1)) (TempPoint(1,2)+distanceY+a1(1,2))];
+##  InitialPoint=TempPoint;
+##  %  FinalPoint=[(InitialPoint(1,1)+a1(1,1)) (InitialPoint(1,2)+a1(1,2))];
+##  
+##  points=[points;FinalPoint];
+##  accelerations=[accelerations;a1];
+##  plot([InitialPoint(1,1) FinalPoint(1,1)],[InitialPoint(1,2) FinalPoint(1,2)],'r')
+##endfor
+
+for i=1:10
+  Xsub2=FinalPoint(1,1);
+  if Xsub2<0
+    Xsub2=Xsub2*-1;
   endif
-  if (theta1<=1.178097 && theta1>=0.3926991)
-    a1=[1 1];
-  elseif (theta1>=0 && theta1<0.3926991)
-    a1=[1 0];
-  elseif (theta1<=1.570796 && theta1>1.178097)
-    a1=[0 1];
+  Ysub2=FinalPoint(1,2);
+  if Ysub2<0
+    Ysub2=Ysub2*-1;
   endif
-  if (FinalPoint(1,2)>0)
-    a1(1,2)=a1(1,2)*-1;
+  DistanceToCenter=sqrt(power(Xsub2,2)+power(Ysub2,2));
+  Thetasub2=asin(Ysub2/DistanceToCenter);
+  Asub1=[0 0];
+  if (Thetasub2<=deg2rad(67.5))&&(Thetasub2>=deg2rad(22.5))
+    Asub1=[1 1];
   endif
-  if (FinalPoint(1,1)>0)
-    a1(1,1)=a1(1,1)*-1;
+  if (Thetasub2>=deg2rad(0))&&(Thetasub2<deg2rad(22.5))
+    Asub1=[1 0];
   endif
-%  InitialPoint=FinalPoint;
-  TempPoint=FinalPoint;
-  FinalPoint=[(FinalPoint(1,1)+(FinalPoint(1,1)-InitialPoint(1,1))+a1(1,1)) (FinalPoint(1,2)+(FinalPoint(1,2)-InitialPoint(1,2))+a1(1,2))];
-  InitialPoint=TempPoint;
-  %  FinalPoint=[(InitialPoint(1,1)+a1(1,1)) (InitialPoint(1,2)+a1(1,2))];
+  if (Thetasub2<=deg2rad(90))&&(Thetasub2>deg2rad(67.5))
+    Asub1=[0 1];
+  endif
+  if FinalPoint(1,2)>0
+    Asub1(1,2)=Asub1(1,2)*-1;
+  endif
+  if FinalPoint(1,1)>0
+    Asub1(1,1)=Asub1(1,1)*-1;
+  endif
   
-  points=[points;FinalPoint];
-  accelerations=[accelerations;a1];
+  totalDistanceX=FinalPoint(1,1)-InitialPoint(1,1);
+  totalDistanceY=FinalPoint(1,2)-InitialPoint(1,2);
+  %DistanceVector=[totalDistanceX totalDistanceY];
+  ClonedVector=[FinalPoint(1,1)+totalDistanceX FinalPoint(1,2)+totalDistanceY];
+  SubmittedVector=[ClonedVector(1,1)+Asub1(1,1) ClonedVector(1,2)+Asub1(1,2)];
+  hold on;
+  plot([FinalPoint(1,1) SubmittedVector(1,1)],[FinalPoint(1,2) SubmittedVector(1,2)],'k')
+  InitialPoint=FinalPoint;
+  FinalPoint=SubmittedVector;
+  points=[points;SubmittedVector];
+  accelerations=[accelerations;Asub1];
 endfor
